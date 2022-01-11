ba475a54adbdc7265bee37ba4b1731475a04cb6c
Simon Lauwers <simon.lauwers@student.kdg.be>
2022-01-11 13:23:51 +0100

    style: 💄 seeding data for demo

--- src/main/kotlin/com/theta/userservice/domain/service/DbInit.kt
+++ src/main/kotlin/com/theta/userservice/domain/service/DbInit.kt
@@ -20,17 +20,17 @@ class DbInit(val roleService: RoleService, val userService: UserService) {
         // seed users
         if(userService.findByEmail("quinten@verhelst.dev") == null){
             userService.save(User(UUID.fromString("aaaaaaaa-aaaa-aaaa-aaaa-aaaaaaaaaaa1"), "quinten@verhelst.dev", BCryptPasswordEncoder().encode("admin"), "quintenvh",
-                    isEnabled = true, isBanned = false, roleService.findByName("admin")!!, "https://pbs.twimg.com/profile_images/1331347002119090179/HAIk7lN2_400x400.jpg", Provider.LOCAL))
+                    isEnabled = true, isBanned = false, roleService.findByName("admin")!!, "https://avatars.dicebear.com/api/micah/${Math.random()}.svg", Provider.LOCAL))
             userService.save(User(UUID.fromString("aaaaaaaa-aaaa-aaaa-aaaa-aaaaaaaaaaa2"), "simon.lauwers@student.kdg.be", BCryptPasswordEncoder().encode("admin"), "simonlauw",
-                    isEnabled = true, isBanned = false, roleService.findByName("admin")!!, "", Provider.LOCAL))
+                    isEnabled = true, isBanned = false, roleService.findByName("admin")!!, "https://avatars.dicebear.com/api/micah/${Math.random()}.svg\"", Provider.LOCAL))
             userService.save(User(UUID.fromString("aaaaaaaa-aaaa-aaaa-aaaa-aaaaaaaaaaa3"), "nathan.tetroashvili@student.kdg.be", BCryptPasswordEncoder().encode("admin"), "nathantetro",
-                    isEnabled = true, isBanned = false, roleService.findByName("admin")!!, "", Provider.LOCAL))
+                    isEnabled = true, isBanned = false, roleService.findByName("admin")!!, "https://avatars.dicebear.com/api/micah/${Math.random()}.svg\"", Provider.LOCAL))
             userService.save(User(UUID.fromString("aaaaaaaa-aaaa-aaaa-aaaa-aaaaaaaaaaa4"), "siemen.vandemosselaer@student.kdg.be", BCryptPasswordEncoder().encode("admin"), "siemenvdm",
-                    isEnabled = true, isBanned = false, roleService.findByName("admin")!!, "", Provider.LOCAL))
+                    isEnabled = true, isBanned = false, roleService.findByName("admin")!!, "https://avatars.dicebear.com/api/micah/${Math.random()}.svg\"", Provider.LOCAL))
             userService.save(User(UUID.fromString("aaaaaaaa-aaaa-aaaa-aaaa-aaaaaaaaaaa5"), "youssef.taouil@student.kdg.be", BCryptPasswordEncoder().encode("admin"), "yousseftao",
-                    isEnabled = true, isBanned = false, roleService.findByName("admin")!!, "", Provider.LOCAL))
+                    isEnabled = true, isBanned = false, roleService.findByName("admin")!!, "https://avatars.dicebear.com/api/micah/${Math.random()}.svg\"", Provider.LOCAL))
             userService.save(User(UUID.fromString("aaaaaaaa-aaaa-aaaa-aaaa-aaaaaaaaaaa6"), "daniel.savin@student.kdg.be", BCryptPasswordEncoder().encode("admin"), "danielsav",
-                    isEnabled = true, isBanned = false, roleService.findByName("admin")!!, "", Provider.LOCAL))
+                    isEnabled = true, isBanned = false, roleService.findByName("admin")!!, "https://avatars.dicebear.com/api/micah/${Math.random()}.svg\"", Provider.LOCAL))
         }
     }
 }
